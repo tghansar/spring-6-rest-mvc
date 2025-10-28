beec0f6367825080b4b54b1689a36b46565d0055
Taariq <tghansar@gmail.com>
2025-10-28 10:22:00 +0200

    Adding integration test for showcasing optimistic locks

--- src/test/java/com/example/spring6restmvc/controllers/CustomerControllerIntegrationTest.java
+++ src/test/java/com/example/spring6restmvc/controllers/CustomerControllerIntegrationTest.java
@@ -1,13 +1,28 @@
 package com.example.spring6restmvc.controllers;
 
+import com.example.spring6restmvc.entities.Beer;
 import com.example.spring6restmvc.exception.NotFoundException;
+import com.example.spring6restmvc.mappers.BeerMapper;
+import com.example.spring6restmvc.model.BeerDto;
+import com.example.spring6restmvc.repositories.BeerRepository;
 import com.example.spring6restmvc.repositories.CustomerRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+//import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author : Taariq
@@ -23,6 +38,25 @@ public class CustomerControllerIntegrationTest {
 
     @Autowired
     CustomerRepository customerRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Autowired
+    private BeerMapper beerMapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
 
     @Test
     void testCustomerIdNotFound() {
@@ -41,5 +75,37 @@ public class CustomerControllerIntegrationTest {
         customerRepository.deleteAll();
         var customers = customerController.getCustomers();
         Assertions.assertThat(customers.size()).isEqualTo(0);
+    }
+
+    @Test
+    void testUpdateBeerBadVersion() throws Exception {
+        Beer beer = beerRepository.findAll().getFirst();
+
+        BeerDto beerDto = beerMapper.beerToBeerDto(beer);
+        beerDto.setBeerName("Updated Name");
+
+        MvcResult result = mockMvc.perform(put(String.format("%s/%s", BeerController.BEER_PATH, beer.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beerDto)))
+//                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isNoContent())
+                .andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+
+        beerDto.setBeerName("Updated Name 2");
+
+        MvcResult result2 = mockMvc.perform(put(String.format("%s/%s", BeerController.BEER_PATH, beer.getId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beerDto)))
+//                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isNoContent())
+                .andReturn();
+
+        System.out.println(result2.getResponse().getStatus());
     }
 }
